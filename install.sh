2c1e25b1453014e8f0b3b50eed92eabf12137685
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2021-02-18 11:22:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -151,7 +151,7 @@ BLOCKCOUNT=0
 
 until su -c "trittium-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" $USER; do
   for (( i=0; i<${#CHARS}; i++ )); do
-    sleep 2
+    sleep 5
     BLOCKCOUNT=`su -l -c "trittium-cli getblockcount" $USER`
 	echo -en "${CHARS:$i:1}" "$BLOCKCOUNT" "\r"
   done
